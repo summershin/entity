6a08340b15d8e1969ed586f0e220ab0731378a20
慶陽 劉 <wsxqaz664422@gmail.com>
2022-05-07 10:13:57 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/UserManagement.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class UserManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Browses = new System.Windows.Forms.Button();
+            this.pic_Photo = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_Age = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_FirstName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Phone = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.txt_Gender = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Photo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(1002, 409);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(82, 34);
+            this.btn_Cancel.TabIndex = 43;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(1002, 353);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(82, 34);
+            this.btn_Save.TabIndex = 42;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Browses
+            // 
+            this.btn_Browses.Location = new System.Drawing.Point(1002, 301);
+            this.btn_Browses.Name = "btn_Browses";
+            this.btn_Browses.Size = new System.Drawing.Size(82, 34);
+            this.btn_Browses.TabIndex = 41;
+            this.btn_Browses.Text = "Browse...";
+            this.btn_Browses.UseVisualStyleBackColor = true;
+            // 
+            // pic_Photo
+            // 
+            this.pic_Photo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_Photo.Location = new System.Drawing.Point(861, 45);
+            this.pic_Photo.Name = "pic_Photo";
+            this.pic_Photo.Size = new System.Drawing.Size(243, 247);
+            this.pic_Photo.TabIndex = 40;
+            this.pic_Photo.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(776, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 20);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "Photo: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(41, 237);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 20);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Address: ";
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Address.Location = new System.Drawing.Point(146, 234);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(384, 31);
+            this.txt_Address.TabIndex = 37;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(573, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "Age: ";
+            // 
+            // txt_Age
+            // 
+            this.txt_Age.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Age.Location = new System.Drawing.Point(638, 179);
+            this.txt_Age.Name = "txt_Age";
+            this.txt_Age.Size = new System.Drawing.Size(73, 31);
+            this.txt_Age.TabIndex = 35;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(545, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 20);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Gender: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(51, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "E-mail: ";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Email.Location = new System.Drawing.Point(146, 176);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(384, 31);
+            this.txt_Email.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(357, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 20);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "First Name: ";
+            // 
+            // txt_FirstName
+            // 
+            this.txt_FirstName.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_FirstName.Location = new System.Drawing.Point(492, 113);
+            this.txt_FirstName.Name = "txt_FirstName";
+            this.txt_FirstName.Size = new System.Drawing.Size(191, 31);
+            this.txt_FirstName.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(21, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Last Name: ";
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_LastName.Location = new System.Drawing.Point(146, 113);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(189, 31);
+            this.txt_LastName.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(354, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Password: *";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Password.Location = new System.Drawing.Point(479, 46);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(204, 31);
+            this.txt_Password.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(38, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Phone: *";
+            // 
+            // txt_Phone
+            // 
+            this.txt_Phone.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Phone.Location = new System.Drawing.Point(133, 46);
+            this.txt_Phone.Name = "txt_Phone";
+            this.txt_Phone.Size = new System.Drawing.Size(202, 31);
+            this.txt_Phone.TabIndex = 24;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 328);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(425, 238);
+            this.dataGridView1.TabIndex = 46;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(549, 328);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(425, 238);
+            this.dataGridView2.TabIndex = 47;
+            // 
+            // txt_Gender
+            // 
+            this.txt_Gender.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Gender.Location = new System.Drawing.Point(638, 234);
+            this.txt_Gender.Name = "txt_Gender";
+            this.txt_Gender.Size = new System.Drawing.Size(73, 31);
+            this.txt_Gender.TabIndex = 48;
+            // 
+            // UserManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 592);
+            this.Controls.Add(this.txt_Gender);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Browses);
+            this.Controls.Add(this.pic_Photo);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_Age);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_FirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_LastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Phone);
+            this.Name = "UserManagement";
+            this.Text = "UserManagement";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Photo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Browses;
+        private System.Windows.Forms.PictureBox pic_Photo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_Age;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_FirstName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_LastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Phone;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox txt_Gender;
+    }
+}
---
+++ WindowsFormsApp1/UserManagement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class UserManagement : Form
+    {
+        public UserManagement()
+        {
+            InitializeComponent();
+
+            var q = dbcontext.Customers;
+
+            dataGridView1.DataSource = q.ToList();
+
+            txt_Phone.DataBindings.Add("Text", q.ToList(), "Phone");
+            txt_Password.DataBindings.Add("Text", q.ToList(), "Password");
+            txt_LastName.DataBindings.Add("Text", q.ToList(), "LastName");
+            txt_FirstName.DataBindings.Add("Text", q.ToList(), "FirstName");
+            txt_Email.DataBindings.Add("Text", q.ToList(), "Email");
+            txt_Age.DataBindings.Add("Text", q.ToList(), "Age");
+            txt_Address.DataBindings.Add("Text", q.ToList(), "Address");
+            txt_Gender.DataBindings.Add("Text", q.ToList(), "Gender");
+        }
+
+        IGOEntities dbcontext = new IGOEntities();
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            var q = dbcontext.Customers.Where(n=>n.Phone = dataGridView1.SelectedRows[6])
+        }
+    }
+}
